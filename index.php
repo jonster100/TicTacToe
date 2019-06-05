bb24138b8c7ccfb10f6b84cf387a6e5dd7bc2a2b
jonster100 <jon_james97@hotmail.com>
2019-06-05 15:07:00 +0100

    Added JQuery interface tests
    These are just tests to sending data to the PHP script.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-	require "Engine.php";
+	/*require "Engine.php";
 	$testing = true;
 	$engine = new Engine();
 	if($testing==true){
@@ -7,6 +7,20 @@
 		$engine->setBoardPosition("player1",0,1);
 		$engine->setBoardPosition("player1",0,2);
 	}
-	echo "The winner is " . $engine->getWinner() . "</b>";
+	echo "The winner is " . $engine->getWinner() . "</b>";*/
+	
+	//testing getting data from jquery2
+	$dataObject = json_decode($_POST['jsonData']);
+	echo $dataObject->xValue;
+	
+	
+	//testing getting POST from JQuery form
+	/*if ( isset( $_POST['submit'] ) ) { // retrieve the form data by using the element's name attributes value as key 
+	$xPos = $_POST['xPos']; 
+	$yPos = $_POST['yPos']; // display the results
+	echo '<h3>Form POST Method</h3>'; 
+	echo ' is ' . $xPos . ' ' . $yPos; 
+	exit; 
+	}*/
 ?>
 
